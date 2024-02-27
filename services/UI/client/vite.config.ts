b778e9408e8b37f01a82f464d9aed03d94a0224a
Hubert Cywka <hcywka@gmail.com>
2024-02-27 18:30:18 +0100

    setup linting

--- services/UI/client/vite.config.ts
+++ services/UI/client/vite.config.ts
@@ -1,10 +1,7 @@
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
-
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: {
-
-  },
-  plugins: [react()],
-})
+    server: {},
+    plugins: [react()],
+});
--- services/UI/server/src/server.ts
+++ services/UI/server/src/server.ts
@@ -1,24 +1,25 @@
-import dotenv from 'dotenv';
-import express, { Response } from 'express';
-import path from 'path';
-
+import dotenv from "dotenv";
+import express, { Response } from "express";
+import path from "path";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
-const staticFilesPath = process.env.STATIC_FILES_DIR ?? '../../ui/dist';
+const staticFilesPath = process.env.STATIC_FILES_DIR ?? "../../ui/dist";
 
 function setResponseHeaders(response: Response) {
     response.set({
-        'Content-Security-Policy': 'default-src: self',
-        'Referrer-Policy': 'no-referrer'
-    })
+        "Content-Security-Policy": "default-src: self",
+        "Referrer-Policy": "no-referrer",
+    });
 }
 
 function init() {
-    app.use(express.static(path.join(__dirname, staticFilesPath), {
-        setHeaders: setResponseHeaders
-    }));
+    app.use(
+        express.static(path.join(__dirname, staticFilesPath), {
+            setHeaders: setResponseHeaders,
+        })
+    );
 
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
@@ -26,4 +27,3 @@ function init() {
 }
 
 init();
-
