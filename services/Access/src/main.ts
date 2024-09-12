5c09ede2ba070409dae7cc283835f5e835a8be63
Hubert Cywka <hcywka@gmail.com>
2024-09-12 20:24:17 +0200

    Remove registration of microservices

--- services/Access/src/main.ts
+++ services/Access/src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from '@/app.module';
 import configuration from '@/config/configuration';
 import { ValidationPipe } from '@nestjs/common';
@@ -8,16 +7,11 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const appConfig = configuration();
 
-    app.connectMicroservice<MicroserviceOptions>({
-        transport: Transport.TCP,
-    });
-
     app.useGlobalPipes(
         new ValidationPipe({ whitelist: true, transform: true }),
     );
 
     await app.listen(appConfig.port);
-    await app.startAllMicroservices();
 }
 
 bootstrap();
