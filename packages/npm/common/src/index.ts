e56e0d7eec37712360d01d6fa98e15abd8ce81c6
Hubert Cywka <hcywka@gmail.com>
2024-09-16 12:23:26 +0200

    Add missing export

--- packages/npm/common/src/index.ts
+++ packages/npm/common/src/index.ts
@@ -1,4 +1,7 @@
 import { loggerOptions, pinoLogger } from "./logger/logger";
+import { pollResourceUntilReady } from "./utils/pollResourceUntilReady";
 
 export { loggerOptions, pinoLogger };
+export { pollResourceUntilReady };
+
 export * from "nestjs-pino";
