02966fa5424856a605e702389184a80a8779c746
Hubert Cywka <hcywka@gmail.com>
2024-09-26 13:27:24 +0200

    Remove helmet, configure security headers in Proxy service instead

--- services/UI/server/src/server.ts
+++ services/UI/server/src/server.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import express, { Response } from "express";
+import express from "express";
 import path from "path";
 
 dotenv.config();
@@ -7,23 +7,9 @@ const app = express();
 const port = process.env.PORT;
 const staticFilesPath = process.env.STATIC_FILES_DIR ?? "../../ui/dist";
 
-function setResponseHeaders(response: Response) {
-    response.set({
-        "Content-Security-Policy": "default-src: self",
-        "Referrer-Policy": "no-referrer",
-    });
-}
-
 function init() {
-    app.use(
-        express.static(path.join(__dirname, staticFilesPath), {
-            setHeaders: setResponseHeaders,
-        })
-    );
-
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-    });
+    app.use(express.static(path.join(__dirname, staticFilesPath)));
+    app.listen(port);
 }
 
 init();
