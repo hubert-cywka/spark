805cb617f9020fa3e20498db33adce9ce419e740
Hubert Cywka <hcywka@gmail.com>
2024-11-03 12:05:03 +0100

    Improve UX of side panel

--- services/Frontend/src/features/layout/components/SidePanel/SidePanel.tsx
+++ services/Frontend/src/features/layout/components/SidePanel/SidePanel.tsx
@@ -11,18 +11,22 @@ import { Logo } from "@/components/Logo/Logo";
 import { AccessGuard } from "@/features/auth/components/AccessGuard";
 import { LogoutButton } from "@/features/auth/components/LogoutButton/LogoutButton";
 import { Navigation } from "@/features/layout/components/Navigation/Navigation";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 import { useTranslate } from "@/lib/i18n/hooks/useTranslate";
 
 export const SidePanel = () => {
     const t = useTranslate();
-    // Hubert: On mobile this component should be hidden by default. We are using css to achieve that to prevent layout shifts, but the user still needs to be able to open the component.
-    const [isCollapsedOnMobile, setIsCollapsedOnMobile] = useState(true);
-    const toggleCollapsedState = () => setIsCollapsedOnMobile((prev) => !prev);
+    // Hubert: On mobile this component should be hidden by default. We are using css to achieve that to prevent layout
+    // shifts, but the user still needs to be able to open the component. It doesn't matter if we set it to 'true' on
+    // initial render on desktop.
+    const [isCollapsed, setIsCollapsed] = useState(true);
+    const toggleCollapsedState = () => setIsCollapsed((prev) => !prev);
+    const isDesktop = useMediaQuery("(min-width: 768px)");
 
     return (
         <div
             className={clsx(styles.container, {
-                [styles.collapsed]: isCollapsedOnMobile,
+                [styles.collapsed]: isCollapsed,
             })}
         >
             <div className={styles.sidePanel}>
@@ -31,14 +35,14 @@ export const SidePanel = () => {
                         size="1"
                         variant="secondary"
                         onPress={toggleCollapsedState}
-                        aria-label={t(`common.navigation.collapseButton.label.${isCollapsedOnMobile ? "show" : "hide"}`)}
+                        aria-label={t(`common.navigation.collapseButton.label.${isCollapsed ? "show" : "hide"}`)}
                     >
                         <IconChevronLeft />
                     </IconButton>
                 </div>
 
                 <Logo className={styles.logo} />
-                <Navigation isDisabled={isCollapsedOnMobile} />
+                <Navigation isDisabled={isCollapsed && !isDesktop} />
 
                 <div className={styles.footer}>
                     <AccessGuard requiredScopes={["browse_as_authenticated"]}>
