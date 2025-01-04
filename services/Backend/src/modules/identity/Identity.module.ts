bef10207ebb020e8898f913e029589519315de6e
Hubert Cywka <hcywka@gmail.com>
2025-01-04 22:23:49 +0100

    Remove database module from shared modules

--- services/Backend/src/modules/identity/Identity.module.ts
+++ services/Backend/src/modules/identity/Identity.module.ts
@@ -1,10 +1,21 @@
 import { Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 
+import { DatabaseModule } from "@/common/database/Database.module";
 import { IInboxEventHandler, InboxEventHandlersToken } from "@/common/events";
+import { InboxEventEntity } from "@/common/events/entities/InboxEvent.entity";
+import { OutboxEventEntity } from "@/common/events/entities/OutboxEvent.entity";
 import { AccountModule } from "@/modules/identity/account/Account.module";
+import { BaseAccountEntity } from "@/modules/identity/account/entities/BaseAccountEntity";
+import { FederatedAccountEntity } from "@/modules/identity/account/entities/FederatedAccountEntity";
+import { ManagedAccountEntity } from "@/modules/identity/account/entities/ManagedAccountEntity";
+import { SingleUseTokenEntity } from "@/modules/identity/account/entities/SingleUseTokenEntity";
 import { AuthenticationModule } from "@/modules/identity/authentication/Authentication.module";
+import { RefreshTokenEntity } from "@/modules/identity/authentication/entities/RefreshToken.entity";
 import { AccountPasswordUpdatedEventHandler } from "@/modules/identity/authentication/events/AccountPasswordUpdatedEvent.handler";
 import { AccessTokenStrategy } from "@/modules/identity/authentication/strategies/passport/AccessToken.strategy";
+import { IDENTITY_MODULE_DATA_SOURCE } from "@/modules/identity/infrastructure/database/constants";
+import { InitializeIdentityModule1735737549567 } from "@/modules/identity/infrastructure/database/migrations/1735737549567-InitializeIdentityModule";
 import { IdentitySharedModule } from "@/modules/identity/shared/IdentityShared.module";
 
 @Module({
@@ -15,7 +26,34 @@ import { IdentitySharedModule } from "@/modules/identity/shared/IdentityShared.m
             inject: [AccessTokenStrategy, AccountPasswordUpdatedEventHandler],
         },
     ],
-    imports: [IdentitySharedModule, AccountModule, AuthenticationModule],
+    imports: [
+        DatabaseModule.forRootAsync(
+            IDENTITY_MODULE_DATA_SOURCE,
+            [
+                RefreshTokenEntity,
+                SingleUseTokenEntity,
+                BaseAccountEntity,
+                ManagedAccountEntity,
+                FederatedAccountEntity,
+                OutboxEventEntity,
+                InboxEventEntity,
+            ],
+            {
+                useFactory: (configService: ConfigService) => ({
+                    port: configService.getOrThrow<number>("modules.identity.database.port"),
+                    username: configService.getOrThrow<string>("modules.identity.database.username"),
+                    password: configService.getOrThrow<string>("modules.identity.database.password"),
+                    host: configService.getOrThrow<string>("modules.identity.database.host"),
+                    database: configService.getOrThrow<string>("modules.identity.database.name"),
+                    migrations: [InitializeIdentityModule1735737549567],
+                }),
+                inject: [ConfigService],
+            }
+        ),
+        IdentitySharedModule,
+        AccountModule,
+        AuthenticationModule,
+    ],
     exports: [],
 })
 export class IdentityModule {}
--- services/Backend/src/modules/journal/Journal.module.ts
+++ services/Backend/src/modules/journal/Journal.module.ts
@@ -1,9 +1,20 @@
 import { Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 
+import { DatabaseModule } from "@/common/database/Database.module";
 import { IInboxEventHandler, InboxEventHandlersToken } from "@/common/events";
+import { InboxEventEntity } from "@/common/events/entities/InboxEvent.entity";
+import { OutboxEventEntity } from "@/common/events/entities/OutboxEvent.entity";
 import { AuthorModule } from "@/modules/journal/author/Author.module";
+import { AuthorEntity } from "@/modules/journal/author/entities/Author.entity";
 import { AccountRegisteredEventHandler } from "@/modules/journal/author/events/AccountRegisteredEvent.handler";
 import { DailyModule } from "@/modules/journal/daily/Daily.module";
+import { DailyEntity } from "@/modules/journal/daily/entities/Daily.entity";
+import { JOURNAL_MODULE_DATA_SOURCE } from "@/modules/journal/infrastructure/database/constants";
+import { InitializeJournalModule1735835917869 } from "@/modules/journal/infrastructure/database/migrations/1735835917869-InitializeJournalModule";
+import { AddDailyEntity1735843548123 } from "@/modules/journal/infrastructure/database/migrations/1735843548123-AddDailyEntity";
+import { AddJournalAuthor1735844848384 } from "@/modules/journal/infrastructure/database/migrations/1735844848384-AddJournalAuthor";
+import { AddAuthorIdColumnInDailyEntity1735994132208 } from "@/modules/journal/infrastructure/database/migrations/1735994132208-AddAuthorIdColumnInDailyEntity";
 import { JournalSubscriber } from "@/modules/journal/Journal.subscriber";
 import { JournalSharedModule } from "@/modules/journal/shared/JournalShared.module";
 
@@ -15,7 +26,27 @@ import { JournalSharedModule } from "@/modules/journal/shared/JournalShared.modu
             inject: [AccountRegisteredEventHandler],
         },
     ],
-    imports: [JournalSharedModule, AuthorModule, DailyModule],
+    imports: [
+        DatabaseModule.forRootAsync(JOURNAL_MODULE_DATA_SOURCE, [OutboxEventEntity, InboxEventEntity, DailyEntity, AuthorEntity], {
+            useFactory: (configService: ConfigService) => ({
+                port: configService.getOrThrow<number>("modules.journal.database.port"),
+                username: configService.getOrThrow<string>("modules.journal.database.username"),
+                password: configService.getOrThrow<string>("modules.journal.database.password"),
+                host: configService.getOrThrow<string>("modules.journal.database.host"),
+                database: configService.getOrThrow<string>("modules.journal.database.name"),
+                migrations: [
+                    InitializeJournalModule1735835917869,
+                    AddDailyEntity1735843548123,
+                    AddJournalAuthor1735844848384,
+                    AddAuthorIdColumnInDailyEntity1735994132208,
+                ],
+            }),
+            inject: [ConfigService],
+        }),
+        JournalSharedModule,
+        AuthorModule,
+        DailyModule,
+    ],
     controllers: [JournalSubscriber],
 })
 export class JournalModule {}
