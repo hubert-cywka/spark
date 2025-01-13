4d8986fa85b2f940a0400c68c9f5f75de596d3e2
Hubert Cywka <hcywka@gmail.com>
2025-01-13 17:49:21 +0100

    Fix

--- services/Frontend/src/types/Icon.d.ts
+++ services/Frontend/src/types/Icon.d.ts
@@ -1,6 +1,8 @@
 import { ForwardRefExoticComponent, RefAttributes } from "react";
 import { Icon } from "lucide-react";
 
-export type IconComponent = ForwardRefExoticComponent<RefAttributes<Icon>> & {
-    className?: string;
-};
+export type IconComponent = ForwardRefExoticComponent<
+    RefAttributes<Icon> & {
+        className?: string;
+    }
+>;
