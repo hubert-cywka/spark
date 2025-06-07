47310ac091dd0a202b37e6df1187f4722d52dd22
Hubert Cywka <hcywka@gmail.com>
2025-06-07 20:35:29 +0200

    Fix

--- services/Frontend/src/components/Calendar/Calendar.tsx
+++ services/Frontend/src/components/Calendar/Calendar.tsx
@@ -7,8 +7,11 @@ import styles from "./styles/Calendar.module.scss";
 import { CalendarProps } from "@/components/Calendar/types/Calendar";
 import { IconButton } from "@/components/IconButton";
 import { formatToISODateString } from "@/features/daily/utils/dateUtils";
+import { useTranslate } from "@/lib/i18n/hooks/useTranslate.ts";
 
 export const Calendar = ({ minDate, maxDate, value, onChange, shownMonths = 1, shownMonthsOffset = 0 }: CalendarProps) => {
+    const t = useTranslate();
+
     const minValue = minDate ? parseDate(minDate) : undefined;
     const maxValue = maxDate ? parseDate(maxDate) : undefined;
     const internalValue = value
@@ -39,9 +42,19 @@ export const Calendar = ({ minDate, maxDate, value, onChange, shownMonths = 1, s
             visibleDuration={{ months: shownMonths }}
         >
             <header className={styles.header}>
-                <IconButton slot="previous" iconSlot={ChevronLeftIcon} />
+                <IconButton
+                    slot="previous"
+                    iconSlot={ChevronLeftIcon}
+                    tooltip={t("common.dateRangePicker.previousRangeButton.label")}
+                    aria-label={t("common.dateRangePicker.previousRangeButton.label")}
+                />
                 <Heading />
-                <IconButton slot="next" iconSlot={ChevronRightIcon} />
+                <IconButton
+                    slot="next"
+                    iconSlot={ChevronRightIcon}
+                    tooltip={t("common.dateRangePicker.nextRangeButton.label")}
+                    aria-label={t("common.dateRangePicker.nextRangeButton.label")}
+                />
             </header>
 
             <div className={styles.gridWrapper}>
