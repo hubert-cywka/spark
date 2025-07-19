9a9fdce2a7c3ec162ecf01f81d366cfa9c73f41a
Hubert Cywka <hcywka@gmail.com>
2025-07-19 12:46:01 +0200

    Add createdAt and updatedAt timestamps to few entities

--- services/Backend/src/modules/gdpr/entities/Tenant.entity.ts
+++ services/Backend/src/modules/gdpr/entities/Tenant.entity.ts
@@ -1,4 +1,4 @@
-import { type Relation, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { type Relation, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 import { DataPurgePlanEntity } from "@/modules/gdpr/entities/DataPurgePlan.entity";
 
@@ -6,6 +6,12 @@ import { DataPurgePlanEntity } from "@/modules/gdpr/entities/DataPurgePlan.entit
 export class TenantEntity {
     @PrimaryGeneratedColumn("uuid")
     id!: string;
+
+    @CreateDateColumn({ type: "timestamptz" })
+    createdAt!: Date;
+
+    @UpdateDateColumn({ type: "timestamptz" })
+    updatedAt!: Date;
 
     @OneToMany(() => DataPurgePlanEntity, (plan) => plan.tenant)
     dataPurgePlans!: Relation<DataPurgePlanEntity[]>;
