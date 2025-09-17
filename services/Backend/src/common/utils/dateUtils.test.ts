239993c86faf2911e52449ec88b5327e186ab1e3
Hubert Cywka <hcywka@gmail.com>
2025-09-17 22:29:51 +0200

    Formatting

--- services/Backend/src/common/utils/dateUtils.test.ts
+++ services/Backend/src/common/utils/dateUtils.test.ts
@@ -2,31 +2,30 @@ import dayjs from "dayjs";
 import timezone from "dayjs/plugin/timezone.js";
 import utc from "dayjs/plugin/utc.js";
 
-import {isOutsideDateRange} from "@/common/utils/dateUtils";
-import {ISODateString, ISODateStringRange} from "@/types/Date";
+import { isOutsideDateRange } from "@/common/utils/dateUtils";
+import { ISODateString, ISODateStringRange } from "@/types/Date";
 
 describe("dateUtils", () => {
-    
     beforeAll(() => {
         dayjs.extend(utc);
         dayjs.extend(timezone);
-    })
-    
+    });
+
     const getISODateString = (offsetDays: number): ISODateString => {
         return dayjs().utc().add(offsetDays, "day").format("YYYY-MM-DD") as ISODateString;
-    }
-    
+    };
+
     describe("isOutsideDateRange", () => {
         it("should return true when the current date is before the range", () => {
             const range: ISODateStringRange = {
                 from: getISODateString(-2),
                 to: getISODateString(-1),
             };
-            
+
             const result = isOutsideDateRange(range);
-            
+
             expect(result).toBe(true);
-        })
+        });
 
         it("should return true when the current date is after the range", () => {
             const range: ISODateStringRange = {
@@ -37,7 +36,7 @@ describe("dateUtils", () => {
             const result = isOutsideDateRange(range);
 
             expect(result).toBe(true);
-        })
+        });
 
         it("should return false when the current date is inside the range", () => {
             const range: ISODateStringRange = {
@@ -48,6 +47,6 @@ describe("dateUtils", () => {
             const result = isOutsideDateRange(range);
 
             expect(result).toBe(false);
-        })
-    })
+        });
+    });
 });
