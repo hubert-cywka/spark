68ddbb01ee728795e690be2c5446842bc56ec9c1
Hubert Cywka <hcywka@gmail.com>
2025-11-19 21:24:01 +0100

    Init empty Scheduler project (#159)
    * dateUtils tests
    
    * Formatting
    
    * Init empty scheduler project

---
+++ services/Backend/src/common/utils/dateUtils.test.ts
@@ -0,0 +1,52 @@
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone.js";
+import utc from "dayjs/plugin/utc.js";
+
+import { isOutsideDateRange } from "@/common/utils/dateUtils";
+import { ISODateString, ISODateStringRange } from "@/types/Date";
+
+describe("dateUtils", () => {
+    beforeAll(() => {
+        dayjs.extend(utc);
+        dayjs.extend(timezone);
+    });
+
+    const getISODateString = (offsetDays: number): ISODateString => {
+        return dayjs().utc().add(offsetDays, "day").format("YYYY-MM-DD") as ISODateString;
+    };
+
+    describe("isOutsideDateRange", () => {
+        it("should return true when the current date is before the range", () => {
+            const range: ISODateStringRange = {
+                from: getISODateString(-2),
+                to: getISODateString(-1),
+            };
+
+            const result = isOutsideDateRange(range);
+
+            expect(result).toBe(true);
+        });
+
+        it("should return true when the current date is after the range", () => {
+            const range: ISODateStringRange = {
+                from: getISODateString(1),
+                to: getISODateString(2),
+            };
+
+            const result = isOutsideDateRange(range);
+
+            expect(result).toBe(true);
+        });
+
+        it("should return false when the current date is inside the range", () => {
+            const range: ISODateStringRange = {
+                from: getISODateString(-1),
+                to: getISODateString(1),
+            };
+
+            const result = isOutsideDateRange(range);
+
+            expect(result).toBe(false);
+        });
+    });
+});
