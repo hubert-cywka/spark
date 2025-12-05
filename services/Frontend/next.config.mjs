621810e3fc0a86308b026472696d7d1a606e6127
Hubert Cywka <hcywka@gmail.com>
2025-12-05 19:58:40 +0100

    Fucking idiots

--- services/Frontend/next.config.mjs
+++ services/Frontend/next.config.mjs
@@ -8,9 +8,43 @@ const nextConfig = {
     distDir: "./.next",
     basePath: "",
     serverExternalPackages: ["pino", "pino-pretty", "thread-stream"],
-    turbopack: {
-        root: "/"
-    }
+    experimental: {
+        sri: {
+            algorithm: "sha256"
+        }
+    },
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "Content-Security-Policy",
+                        value: buildCSPConfig(),
+                    },
+                ],
+            },
+        ]
+    },
 };
 
 export default withNextIntl(nextConfig);
+
+export const buildCSPConfig = () => {
+    const apiURL = process.env.NEXT_PUBLIC_API_URL;
+
+    const cspHeader = `
+    default-src 'self';
+    script-src 'self';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    connect-src 'self' ${apiURL};
+    upgrade-insecure-requests;
+`;
+    return cspHeader.replace(/\s{2,}/g, " ").trim();
+};
