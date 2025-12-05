0b5f880d30ec3575306499865c1d9a72cb76c2e2
Hubert Cywka <hcywka@gmail.com>
2025-12-05 20:06:05 +0100

    Shame on you vercel

--- services/Frontend/next.config.mjs
+++ services/Frontend/next.config.mjs
@@ -6,45 +6,7 @@ const withNextIntl = createNextIntlPlugin("./src/lib/i18n/request.ts");
 const nextConfig = {
     output: "standalone",
     distDir: "./.next",
-    basePath: "",
-    serverExternalPackages: ["pino", "pino-pretty", "thread-stream"],
-    experimental: {
-        sri: {
-            algorithm: "sha256"
-        }
-    },
-    async headers() {
-        return [
-            {
-                source: "/(.*)",
-                headers: [
-                    {
-                        key: "Content-Security-Policy",
-                        value: buildCSPConfig(),
-                    },
-                ],
-            },
-        ]
-    },
+    basePath: ""
 };
 
 export default withNextIntl(nextConfig);
-
-export const buildCSPConfig = () => {
-    const apiURL = process.env.NEXT_PUBLIC_API_URL;
-
-    const cspHeader = `
-    default-src 'self';
-    script-src 'self';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    connect-src 'self' ${apiURL};
-    upgrade-insecure-requests;
-`;
-    return cspHeader.replace(/\s{2,}/g, " ").trim();
-};
