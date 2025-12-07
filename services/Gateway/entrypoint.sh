1c170a6cd5a4217445ce0acbab3ead5e116c1916
Hubert Cywka <hcywka@gmail.com>
2025-12-07 22:00:09 +0100

    Use distroless envoy image (#168)

--- services/Gateway/entrypoint.sh
+++ services/Gateway/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-cat /envoy.config.yaml.template | envsubst > /etc/envoy.config.yaml
-echo "Waiting for Gateway service to start..."
-/usr/local/bin/envoy -c /etc/envoy.config.yaml --use-dynamic-base-id --log-format "{\"level\": \"%l\", \"timestamp\": \"%Y-%m-%dT%T.%eZ\", \"category\": \"%n\", \"source\":\"%g:%#\", \"msg\": \"%j\"}"
+
+set -e
+/usr/local/bin/envsubst < /envoy.config.yaml.template > /tmp/envoy.config.yaml
+exec /usr/local/bin/envoy -c /tmp/envoy.config.yaml \
+    --use-dynamic-base-id \
+    --log-format "{\"level\": \"%l\", \"timestamp\": \"%Y-%m-%dT%T.%eZ\", \"category\": \"%n\", \"source\":\"%g:%#\", \"msg\": \"%j\"}"
