dcf0580dfac515aae22dd52feaf40773032b5db3
Hubert Cywka <hcywka@gmail.com>
2025-12-08 22:35:13 +0100

    Test

--- services/Backend/src/common/s2s/ServiceToService.module.ts
+++ services/Backend/src/common/s2s/ServiceToService.module.ts
@@ -29,6 +29,9 @@ export class ServiceToServiceModule {
                     inject: options.inject || [],
                 },
             ],
+            
+            
+            
             exports: [ServiceToServiceClientToken],
         };
     }
