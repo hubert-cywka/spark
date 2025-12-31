205e5356c03ad2a0cd4ab97f6b661a9801d6773f
Hubert Cywka <hcywka@gmail.com>
2025-12-31 17:10:13 +0100

    Update button style

--- services/Frontend/src/features/export/components/ExportEntryItem/ExportEntryItem.tsx
+++ services/Frontend/src/features/export/components/ExportEntryItem/ExportEntryItem.tsx
@@ -71,7 +71,7 @@ export const ExportEntryItem = ({ entry }: ExportEntryItemProps) => {
                     <IconButton
                         aria-label={t("exports.list.buttons.download.label")}
                         tooltip={t("exports.list.buttons.download.label")}
-                        variant="primary"
+                        variant="subtle"
                         onClick={() => handleRequestExportFiles(entry.id)}
                         isLoading={isRequestingFiles}
                         iconSlot={DownloadIcon}
