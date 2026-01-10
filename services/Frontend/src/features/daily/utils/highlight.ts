69ad1341dd4d4f0d7653a0b67916cd687caa9517
Hubert Cywka <hcywka@gmail.com>
2026-01-10 20:59:06 +0100

    Save

--- services/Frontend/src/features/daily/utils/highlight.ts
+++ services/Frontend/src/features/daily/utils/highlight.ts
@@ -5,7 +5,7 @@ const HIGHLIGHT_DURATION = 3000;
 const HIGHLIGHT_CLASSNAME = styles.element;
 
 export const highlightElement = (element: Element) => {
-    element.scrollIntoView({ behavior: "smooth", block: "nearest" });
+    element.scrollIntoView({ behavior: "smooth", block: "start" });
 
     element.classList.add(HIGHLIGHT_CLASSNAME);
     element.setAttribute(DATA_HIGHLIGHTED_ATTRIBUTE_NAME, "true");
